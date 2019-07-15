4d5b8648ce489b02309a96769f4f84c57c5c9758
18871002288 <18871002288@163.com>
2019-07-15 12:08:57 +0800

    PlatformView显示原生视图插件

---
+++ 第13章Flutter插件开发/nativeweb/example/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:nativeweb/nativeweb.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  //定义WebController对象
+  WebController webController;
+  //定义NativeWeb插件对象
+  NativeWeb nativeWeb;
+
+  @override
+  void initState() {
+    super.initState();
+    //创建NativeWeb插件对象 View创建完成提供回调方法
+    nativeWeb = NativeWeb(onWebCreated: onWebCreated,);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('PlatformView示例'),
+        ),
+        body: Container(
+          //渲染插件显示加载的网页内容
+          child: nativeWeb,
+          height: 600.0,
+        ),
+      ),
+    );
+    
+  }
+
+  //执行网页加载动作
+  void onWebCreated(webController){
+    this.webController = webController;
+    this.webController.loadUrl("https://www.baidu.com");
+  }
+  
+  
+}
---
+++ 第13章Flutter插件开发/nativeweb/ios/Classes/NativewebPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+@interface NativewebPlugin : NSObject<FlutterPlugin>
+@end
+
